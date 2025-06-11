222b0d4c166eb0ee7eda4872f883d76f837babc2
Antonio <antoniostephens202@gmail.com>
2025-06-10 23:58:10 -0500

    Corrigiendo puerto en Flask

--- app.py
+++ app.py
@@ -69,6 +69,7 @@ def modelo_a_diccionario(tarea):
     }
 
 # 📌 Punto de entrada
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
+    
     
